e51cb42456109ac657ead99af2445798823df7b2
roiqd03 <roiquint@ucm.es>
2023-02-12 21:38:21 +0100

    avance en ataque, detección y movimiento

--- PR2/Ott - An Elemental Journey/gameflow/PlayState.h
+++ PR2/Ott - An Elemental Journey/gameflow/PlayState.h
@@ -11,8 +11,11 @@ private:
 	int i = 0;
 public:
 	PlayState(int stateID, SDLApplication* app) : GameState(stateID,app) {
-		enemy = new Enemy(Vector2D(400, 400), app->getTexture("enemy", stateID), 20, elementsInfo::Earth, nullptr, Vector2D(1,0), Scale(0.5,0.5));
+		enemy = new Enemy(Vector2D(600, 400), app->getTexture("enemy", stateID), 5, elementsInfo::Earth, nullptr, false, Vector2D(1, 0), Scale(0.5, 0.5));
 		gameObjects.push_back(enemy);
+		enemy = new Enemy(Vector2D(300, 400), app->getTexture("enemy", stateID), 20, elementsInfo::Earth, enemy, true, Vector2D(1,0), Scale(0.5,0.5));
+		gameObjects.push_back(enemy);
+
 	}
 
 	virtual void update() {
--- PR2/Ott - An Elemental Journey/gameobjects/Enemy.h
+++ PR2/Ott - An Elemental Journey/gameobjects/Enemy.h
@@ -16,34 +16,41 @@ private:
 
 	const uint PREPARING_TIME = 500;
 	const uint ATTACKING_TIME = 500;
-	const uint NEW_DIR = 1000;
+	const uint NEW_DIR = 500;
 	int startAttackingTime = 0;
 	int startMovingTime = 0;
 	SDL_Rect attackTrigger;
 	triggerState attackState;
 	SDL_Rect detectingTrigger;
 	bool detectPlayer;
+	bool movee;  // TEMPORAL PARA PRUEBAS, BORRAR TODO LO RELACIONADO CON ESTO
+	bool lookingRight = true;
+	const int nearDistance = 50;
 
 	GameObject* player;
 public:
-	Enemy(const Vector2D& position, Texture* texture, int lives, elementsInfo::elements elem, GameObject* p, Vector2D dir = Vector2D(0,0), const Scale& scale = Scale(1.0f, 1.0f), float w = 1.0f, float h = 1.0f): MovingObject(position, texture, dir, scale), actualLives(lives), element(elem), player(p) {
+	Enemy(const Vector2D& position, Texture* texture, int lives, elementsInfo::elements elem, GameObject* p, bool moving, Vector2D dir = Vector2D(0,0), const Scale& scale = Scale(1.0f, 1.0f), float w = 110.0f, float h = 100.0f): MovingObject(position, texture, dir, scale), actualLives(lives), element(elem), player(p) {
 		maxLives = lives * 2;  // Representación interna doblada
 		actualLives = lives * 2;
 		speed = 0.1;
 
-		attackTrigger.x = position.getX() - w; attackTrigger.y = position.getY();
+		attackTrigger.x = position.getX() + width; attackTrigger.y = position.getY();
 		attackTrigger.w = w; attackTrigger.h = h;
 		attackState = normal;
 
-		detectingTrigger.x = position.getX() - 2 * w; detectingTrigger.y = position.getY();
+		detectingTrigger.x = position.getX() + width; detectingTrigger.y = position.getY();
 		detectingTrigger.w = 2 * w; detectingTrigger.h = h;
 		detectPlayer = false;
+
+		movee = moving;
+		player = p;
 	}
 	~Enemy() {
 
 	}
 
 	void Damage(elementsInfo::elements e) {
+		cout << "Hit" << endl;
 		actualLives -= elementsInfo::matrix[e][element];
 		if (actualLives <= 0) Die();
 	}
@@ -54,47 +61,78 @@ public:
 		// borrar o marcar booleano para borrar después
 	}
 
-	void OnEnterAttack() {
-		if (attackState == normal) {
-			attackState = preparing;
+	void DetectPlayer() {
+		SDL_Rect playerRect = player->getRect();
+		int frameTime = SDL_GetTicks() - startAttackingTime;
+		if (!detectPlayer) {
+			if(SDL_HasIntersection(&detectingTrigger, &playerRect)) detectPlayer = true;
+			if(detectPlayer) cout << "Detected" << endl;
 		}
-		else if (attackState == attacking) {
-			//Player.Damage(element) supongo
+		else {
+			if (attackState == normal && SDL_HasIntersection(&attackTrigger, &playerRect)) {
+				attackState = preparing;
+				startAttackingTime = SDL_GetTicks();
+			}
+			else if (attackState == preparing && frameTime >= PREPARING_TIME) {
+				cout << attackState << endl;
+				attackState = attacking;
+				startAttackingTime = SDL_GetTicks();
+			}
+			else if (attackState == attacking && frameTime >= ATTACKING_TIME) {
+				cout << attackState << endl;
+				if (SDL_HasIntersection(&attackTrigger, &playerRect))
+					static_cast<Enemy*>(player)->Damage(element);
+				attackState = normal;
+			}
 		}
 	}
 
-	void OnEnterDetection() {
-		dir = (player->getRect().x - dir.getX(), dir.getY());
-		dir.normalize();
-		detectPlayer = true;
+	void FollowPlayer() {
+		// Distancia de seguridad para comprobar que no se pega al jugador
+		if (lookingRight && abs(player->getRect().x - (position.getX() + width)) > nearDistance ||
+			!lookingRight && abs(player->getRect().x - position.getX()) > nearDistance) {
+			dir = { player->getRect().x - dir.getX(), dir.getY() };
+			dir.normalize();
+		}
+		else {
+			dir = { 0, dir.getY()};
+			dir.normalize();
+		}
 	}
 
 	virtual void update() { //Falta el movimiento del enemigo
-		int frameTime =  SDL_GetTicks() - startAttackingTime;  // Es importante que el orden sea 
-		// SDL_GetTicks() - startAttackingTime y no al revés porque sería negativo y nunca se cumpliría
-		int frameMovingTime = SDL_GetTicks() - startMovingTime;
-		if (attackState == preparing && frameTime >= PREPARING_TIME) {
-			attackState = attacking;
-			startAttackingTime = SDL_GetTicks();
-		}
-		else if (attackState == attacking && frameTime >= ATTACKING_TIME) {
-			attackState = normal;
-			startAttackingTime = SDL_GetTicks();
+		if (!movee) {
+			return;
 		}
 
-		if (dir.getX() > 0) // Ajuste del trigger en función del movimiento del enemigo
+		DetectPlayer();
+		int frameMovingTime = SDL_GetTicks() - startMovingTime;
+
+		if (lookingRight) // Ajuste del trigger en función del movimiento del enemigo
 			attackTrigger.x = position.getX() + width;
 		else
 			attackTrigger.x = position.getX() - attackTrigger.w;
 
+
 		attackTrigger.y = position.getY();
 
-		if (!detectPlayer && frameMovingTime >= NEW_DIR) {
-			dir = { (double)(rand() % 3 - 1), dir.getY()};  // No se puede declarar con parentesis, 
-			// tiene que ser con llaves o con Vector2D(rand() % 3 - 1, dir.getY()).
-			// El módulo tiene que ser entre 3 para poder tener 3 valores, (0, 1, 2). Al restar -1 se queda en (-1, 0, 1)
-			dir.normalize();
-			startMovingTime = SDL_GetTicks();
+		detectingTrigger.x = attackTrigger.x;
+		detectingTrigger.y = attackTrigger.y;
+
+		if (!detectPlayer) {
+			if (frameMovingTime >= NEW_DIR) {
+				dir = { (double)(rand() % 3 - 1), dir.getY()};  // No se puede declarar con parentesis, 
+				// tiene que ser con llaves o con Vector2D(rand() % 3 - 1, dir.getY()).
+				// El módulo tiene que ser entre 3 para poder tener 3 valores, (0, 1, 2). Al restar -1 se queda en (-1, 0, 1)
+				dir.normalize();
+
+				if (dir.getX() < 0) lookingRight = false; // Si es 0 se queda mirando según su último movimiento
+				else if (dir.getX() > 0) lookingRight = true;
+				startMovingTime = SDL_GetTicks();
+			}
+		}
+		else {
+			FollowPlayer();
 		}
 
 		Move();
