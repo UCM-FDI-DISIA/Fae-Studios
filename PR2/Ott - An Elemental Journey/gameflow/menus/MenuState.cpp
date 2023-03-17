79957c4e3edaab426da1b06f58ab63e44a1a3628
DavidRainder <davirive@ucm.es>
2023-03-17 09:53:11 +0100

    probando

--- PR2/Ott - An Elemental Journey/gameflow/menus/MenuState.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../checkML.h"
-#include "MenuState.h"
-#include "../../SDLApplication.h"
-
-void MenuState::render() const {
-    menuBg->render(app->getWindowRect());
-    GameState::render();
-}
