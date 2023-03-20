c67d2848f967fbc7465141461a3e3a5e65bd2d25
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2023-03-20 21:07:05 +0100

    las vines van y vuelven pero no definitivo probably

--- PR2/Ott - An Elemental Journey/src/components/GrowVine.cpp
+++ PR2/Ott - An Elemental Journey/src/components/GrowVine.cpp
@@ -2,17 +2,30 @@
 
 void GrowVine::update() {
     if (orientation == "vertical") {
-        if (dir < 0 && tr_->getPosition().getY() > posFinalT.getY() || dir > 0 && tr_->getPosition().getY() < posFinalT.getY()) {
+        if (!reached && dir < 0 && tr_->getPosition().getY() > posFinalT.getY() || !reached && dir > 0 && tr_->getPosition().getY() < posFinalT.getY()) {
             tr_->setPosition(Vector2D(tr_->getPosition().getX(), tr_->getPosition().getY() + (dir * speed)));
+        }
+        else if (!reached && dir < 0 && tr_->getPosition().getY() <= posFinalT.getY() || !reached && dir > 0 && tr_->getPosition().getY() >= posFinalT.getY()) {
+            reached = true;
+        }
+        else if (reached && goesBack && tr_->getPosition().getY() != posIni.getY()) {
+            tr_->setPosition(Vector2D(tr_->getPosition().getX(), tr_->getPosition().getY() + ((-1*dir) * speed)));
         }
     }
     else {
-        if (dir < 0 && tr_->getPosition().getX() > posFinalT.getX() || dir > 0 && tr_->getPosition().getX() < posFinalT.getX()) {
+        if (!reached && dir < 0 && tr_->getPosition().getX() > posFinalT.getX() || !reached && dir > 0 && tr_->getPosition().getX() < posFinalT.getX()) {
             tr_->setPosition(Vector2D(tr_->getPosition().getX() + (dir * speed), tr_->getPosition().getY()));
+        }
+        else if (!reached && dir < 0 && tr_->getPosition().getX() <= posFinalT.getX() || !reached && dir > 0 && tr_->getPosition().getX() >= posFinalT.getX()) {
+            reached = true;
+        }
+        else if (reached && goesBack && tr_->getPosition().getX() != posIni.getX()) {
+            tr_->setPosition(Vector2D(tr_->getPosition().getX() + ((-1*dir) * speed), tr_->getPosition().getY()));
         }
     }
 }
 void GrowVine::initComponent()
 {
     tr_= ent_->getComponent<Transform>();
+    posIni = ent_->getComponent<Transform>()->getPosition();
 }
--- PR2/Ott - An Elemental Journey/src/components/GrowVine.h
+++ PR2/Ott - An Elemental Journey/src/components/GrowVine.h
@@ -12,13 +12,16 @@
 class GrowVine : public Component
 {
 private:
+	Vector2D posIni;
 	Vector2D posFinalT;
 	Transform* tr_;
 	std::string orientation;
+	bool goesBack;
+	bool reached = false;
 	int speed;
 	int dir;
 public:
-	GrowVine(Vector2D posT, int s, int d, std::string o) : posFinalT(posT), speed(s), dir(d), orientation(o), tr_(nullptr) {
+	GrowVine(Vector2D posT, int s, int d, std::string o, bool back) : posFinalT(posT), speed(s), dir(d), orientation(o), goesBack(back), tr_(nullptr) {
 	}
 	virtual ~GrowVine() {}
 	constexpr static ecs::cmpId_type id = ecs::_GROWVINE;
