2bfb951b4aa8e6ef92cc4d0dee23ed46689e0ac5
DavidRainder <davirive@ucm.es>
2023-03-25 21:28:31 +0100

    cambio de nivel para que no pete lo siento

--- PR2/Ott - An Elemental Journey/src/components/MapComponent.cpp
+++ PR2/Ott - An Elemental Journey/src/components/MapComponent.cpp
@@ -7,7 +7,7 @@
 #include "../states/GameStateMachine.h"
 #include "../game/Constructors.h"
 
-const std::string currentLevel = "level1_1";
+const std::string currentLevel = "level1_0";
 
 std::vector<std::string> strSplit(std::string s, char c) {
 
@@ -86,7 +86,6 @@ void MapComponent::generateEnemies() {
     for (auto it : enemies_[currentRoom]) {
         it->setActive(true);
     }
-
 }
 
 MapComponent::MapComponent(Entity* fadeOut, PlayState* game) : fadeOut(fadeOut), game(game) {
