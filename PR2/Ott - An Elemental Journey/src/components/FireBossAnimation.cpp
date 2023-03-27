1537adf1fde04fd361723adbf3d9602425aa6364
efeliua <100871802+efeliua@users.noreply.github.com>
2023-03-27 12:10:15 +0200

    componentes boss fuego

---
+++ PR2/Ott - An Elemental Journey/src/components/FireBossAnimation.cpp
@@ -0,0 +1,59 @@
+#include "FireBossAnimation.h"
+#include "FramedImage.h"
+void FireBossAnimation::initComponent()
+{
+	image = ent_->getComponent<FramedImage>();
+	eFireBossComponent_ = ent_->getComponent<FireBossComponent>();
+	timer_ = 0;
+	setState(IDLE_FIREBOSS);
+	image->setCol(0); image->setRow(0);
+}
+void FireBossAnimation::update()
+{
+	int c = getColNum(currentAnimation);
+	
+	int state = currentAnimation;
+	timer_++;
+	int col = (timer_ / getTPerFrame(state)) % getNFrames(state) + getColNum(state);
+	image->setCol(col);
+	image->setRow(getRowNum(state));
+	//std::cout << currentAnimation << std::endl;
+	if ((timer_ > (getTPerFrame(state) * getNFrames(state)) + 1)) {// si el timer ha superado al tiempo de cada frame * los frames 
+		endAnim();													// que tiene la animación actual, se llama a endAnim();
+	}
+
+}
+
+void FireBossAnimation::endAnim()
+{
+	if (currentAnimation == DIE_FIREBOSS) {
+		ent_->setAlive(false);
+	}
+	if (currentAnimation == ATTACK_FIREBOSS) {
+		auto fComp = ent_->getComponent<FireBossComponent>();
+		if (fComp->getCurrentCombo() > 0) 
+		{
+			fComp->minusCombo();
+			std::cout << fComp->getCurrentCombo() << std::endl;
+			timer_ = 0;
+			//currentAnimation = IDLE_FIREBOSS;
+			//currentAnimation = ATTACK_FIREBOSS;
+		}
+		else 
+		{
+			if (fComp->isMoving()) currentAnimation = AMBUSH_FIREBOSS;
+			else currentAnimation = IDLE_FIREBOSS;
+		}
+	}
+	
+	
+}
+
+void FireBossAnimation::setState(int newState)
+{
+	if (currentAnimation != newState) {
+		currentAnimation = newState;
+		timer_ = 0;
+	}
+}
+
---
+++ PR2/Ott - An Elemental Journey/src/components/FireBossComponent.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "../ecs/Component.h"
+#include "Transform.h"
+class FireBossAnimation;
+
+class FireBossComponent:public Component
+{ 
+private:
+	Transform* tr_;
+	Entity* player;
+	FireBossAnimation* fAnim_;
+	PhysicsComponent* p;
+	int comboN = 3;
+	int currentCombo=0;
+	//SDL_Rect collider;
+	int specialAttackTimer = 0, timeSpecialAttack = 7, normalAttackTimer = 0, timeNormalAttack = 12, ambushSpeed = 1, speed=0, rSpeed=1,stunTimer = 0, timeStunned = 5, comboTimer = 0, comboTime = 3;
+	bool ambushing = false, retirada = false, stunned = false, combo = false;
+	void startSpecialAttack();
+	void startNormalAttack();
+	void spawnPillars();
+	void shootAtPlayer();
+public:
+	FireBossComponent();
+	virtual void initComponent();
+	virtual void update();
+	void ambush();
+	inline int getCurrentCombo() { return currentCombo; }
+	inline void minusCombo() { currentCombo--; }
+	void stunBoss();
+	inline bool isMoving() { return ambushing || retirada; }
+	inline void setAnimComponent(FireBossAnimation* a) { fAnim_ = a; }
+	constexpr static ecs::cmpId_type id = ecs::_BOSS;
+};
+
