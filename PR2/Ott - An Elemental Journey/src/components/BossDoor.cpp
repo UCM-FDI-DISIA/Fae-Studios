622bd09e1f51a1dba3b787f65654097f74e82c08
raul284 <raulpe07@ucm.es>
2023-04-16 19:24:24 +0200

    santuarios se encienden al activarse

--- PR2/Ott - An Elemental Journey/src/components/BossDoor.cpp
+++ PR2/Ott - An Elemental Journey/src/components/BossDoor.cpp
@@ -1,25 +1,25 @@
-#include "BossDoor.h"
-#include "../sdlutils/InputHandler.h"
-#include "PlayerInput.h"
-
-void BossDoor::initComponent() {
-	player = mngr_->getPlayer();
-	image = ent_->getComponent<Image>();
-	player->getComponent<PlayerInput>()->getDoor(ent_);
-}
-void BossDoor::unlockElem(ecs::elements elem)
-{
-	if (elem == ecs::Earth)
-	{
-		image->changeText(&sdlutils().images().at("BossDoor_Earth"));
-	}
-	else if (elem == ecs::Water)
-	{
-		image->changeText(&sdlutils().images().at("BossDoor_Water"));
-	}
-	else if (elem == ecs::Fire)
-	{
-		image->changeText(&sdlutils().images().at("BossDoor_Fire"));
-		open = true;
-	}
-}
+//#include "BossDoor.h"
+//#include "../sdlutils/InputHandler.h"
+//#include "PlayerInput.h"
+//
+//void BossDoor::initComponent() {
+//	player = mngr_->getPlayer();
+//	image = ent_->getComponent<Image>();
+//	player->getComponent<PlayerInput>()->getDoor(ent_);
+//}
+//void BossDoor::unlockElem(ecs::elements elem)
+//{
+//	if (elem == ecs::Earth)
+//	{
+//		image->changeText(&sdlutils().images().at("BossDoor_Earth"));
+//	}
+//	else if (elem == ecs::Water)
+//	{
+//		image->changeText(&sdlutils().images().at("BossDoor_Water"));
+//	}
+//	else if (elem == ecs::Fire)
+//	{
+//		image->changeText(&sdlutils().images().at("BossDoor_Fire"));
+//		open = true;
+//	}
+//}
