b3f58aab7d8975c0a39e93cf3a82d1037a07df52
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2023-04-18 17:03:38 +0200

    MAPA FINAL TIERRA

--- PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
+++ PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
@@ -1,12 +1,12 @@
-sanctuaryID 0
+sanctuaryID 13
 lifeShards 0 _
 earth 0
 water 0
 fire 0
-map_key level1_1 0
+map_key earthMap 0
 earth_boss 1
 water_boss 1
 fire_boss 1
-earth_map_rooms 0 1 1 0 2 1 3 0 4 0 5 0 6 0 7 0 8 0 9 0 _
+earth_map_rooms 0 1 1 1 2 0 3 0 4 0 5 0 6 0 7 0 8 0 9 0 10 0 11 0 12 0 13 1 _
 water_map_rooms _
 fire_map_rooms _
