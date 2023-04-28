f7809026127d4bf85ca2ba0a72f76adc8d0ef23c
DavidRainder <davirive@ucm.es>
2023-04-28 12:09:14 +0200

    cámara entre salas ya no va tinki winki

--- PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
+++ PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
@@ -1,4 +1,4 @@
-sanctuaryID 1
+sanctuaryID 3
 lifeShards 0 _
 earth 1
 water 1
@@ -7,6 +7,6 @@ map_key earthMap 0
 earth_boss 1
 water_boss 1
 fire_boss 1
-earth_map_rooms 0 1 1 1 2 0 3 0 4 0 5 0 6 0 7 0 8 0 9 0 10 0 11 0 12 0 13 0 _
+earth_map_rooms 0 1 1 1 2 0 3 0 4 0 5 0 6 0 7 1 8 1 9 0 10 1 11 0 12 1 13 0 _
 water_map_rooms _
 fire_map_rooms _
