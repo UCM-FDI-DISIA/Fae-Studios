cbd6250bca62266edee33c27a77c645c2d69ea4f
efeliua <100871802+efeliua@users.noreply.github.com>
2023-04-29 23:50:58 +0200

    cambios testing mapa agua (i)

--- PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
+++ PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
@@ -1,12 +1,12 @@
-sanctuaryID 4
+sanctuaryID 6
 lifeShards 0 _
 earth 1
 water 1
 fire 1
-map_key earthMap 0
+map_key waterMap 0
 earth_boss 1
 water_boss 1
 fire_boss 1
-earth_map_rooms 0 1 1 1 2 0 3 0 4 0 5 0 6 0 7 0 8 0 9 0 10 0 11 0 12 0 13 1 _
+earth_map_rooms 0 1 1 1 2 1 3 1 4 1 5 1 6 0 7 0 8 0 9 0 10 0 11 0 12 0 13 0 14 0 15 0 _
 water_map_rooms _
 fire_map_rooms _
