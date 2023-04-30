d69ce5ea1b9ff9b1e70e19028eeb75e6241554ec
Paula López <paulal18@ucm.es>
2023-04-30 19:00:04 +0200

    mas arreglos spike

--- PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
+++ PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
@@ -1,12 +1,12 @@
-sanctuaryID 0
-lifeShards 0 _
-earth 0
+sanctuaryID 3
+lifeShards 1 3 _
+earth 1
 water 0
 fire 0
 map_key earthMap 0
 earth_boss 1
 water_boss 1
 fire_boss 1
-earth_map_rooms 0 1 1 1 2 0 3 0 4 0 5 0 6 0 7 0 8 0 9 0 10 0 11 0 12 0 13 0 _
+earth_map_rooms 0 1 1 1 2 1 3 1 4 1 5 0 6 1 7 1 8 1 9 1 10 0 11 1 12 1 13 1 _
 water_map_rooms _
 fire_map_rooms _
