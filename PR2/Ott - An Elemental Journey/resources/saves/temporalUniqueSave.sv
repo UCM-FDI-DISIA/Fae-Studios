3142810fdf1c53153c0ed64322ceaa74934b29d8
ClaudiaZarzuela <100291375+ClaudiaZarzuela@users.noreply.github.com>
2023-05-01 01:47:38 +0200

    Mapa de Fuego Final

--- PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
+++ PR2/Ott - An Elemental Journey/resources/saves/temporalUniqueSave.sv
@@ -1,4 +1,4 @@
-sanctuaryID 3
+sanctuaryID 5
 lifeShards 0 _
 earth 1
 water 1
@@ -7,6 +7,6 @@ map_key fireMap 0
 earth_boss 1
 water_boss 1
 fire_boss 1
-earth_map_rooms 0 1 1 1 2 0 3 0 4 0 5 0 6 0 7 0 8 0 9 0 10 0 _
+earth_map_rooms 0 1 1 1 2 0 3 0 4 0 5 1 6 0 7 1 8 1 9 0 10 1 11 0 _
 water_map_rooms _
 fire_map_rooms _
