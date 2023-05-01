d73d93a085504de9f044a85cec6c920307b0976f
Paula López <paulal18@ucm.es>
2023-05-01 20:38:12 +0200

    adicción de todas las spikes

--- PR2/Ott - An Elemental Journey/src/components/MapComponent.cpp
+++ PR2/Ott - An Elemental Journey/src/components/MapComponent.cpp
@@ -587,32 +587,44 @@ void MapComponent::loadMap(std::string path, int nextPos) {
                 newGrass->getComponent<InteractionComponent>()->setIt(it, v);
                 newGrass->setActive(false);
             }
+
             else if (ot.getClass() == "Spike") {
                 auto roomScale = vectorTiles[std::stoi(ot.getName())].first;
                 auto newSpike = constructors::damageArea(mngr_,
-                    "whiteBox", ecs::Earth, (x_ * scale) * roomScale,
-                    ((y_ * scale - sdlutils().images().at("grass").height()) + h_ * scale) * roomScale,
+                    "spike", ecs::Earth, (x_ * scale) * roomScale,
+                    ((y_ * scale - sdlutils().images().at("spike").height()) + h_ * scale) * roomScale,
                     w_ * scale * roomScale,
                     h_ * scale * roomScale,
                     false);
                 interact[std::stoi(ot.getName())].push_back(newSpike);
                 auto v = &interact[std::stoi(ot.getName())];
                 auto it = (--v->end());
-                //newSpike->getComponent<InteractionComponent>()->setIt(it, v);
                 newSpike->setActive(false);
             }
             else if (ot.getClass() == "HangingSpike") {
                 auto roomScale = vectorTiles[std::stoi(ot.getName())].first;
                 auto newSpike = constructors::damageArea(mngr_,
-                    "whiteBox", ecs::Earth, (x_ * scale) * roomScale,
-                    ((y_ * scale - sdlutils().images().at("grass").height()) + h_ * scale) * roomScale,
+                    "hanging_spike", ecs::Earth, (x_ * scale) * roomScale,
+                    ((y_ * scale - sdlutils().images().at("hanging_spike").height()) + h_ * scale) * roomScale,
                     w_ * scale * roomScale,
                     h_ * scale * roomScale,
                     false);
                 interact[std::stoi(ot.getName())].push_back(newSpike);
                 auto v = &interact[std::stoi(ot.getName())];
                 auto it = (--v->end());
-                //newSpike->getComponent<InteractionComponent>()->setIt(it, v);
+                newSpike->setActive(false);
+            }
+            else if (ot.getClass() == "Earth_Spike") {
+                auto roomScale = vectorTiles[std::stoi(ot.getName())].first;
+                auto newSpike = constructors::damageArea(mngr_,
+                    "earth_spike", ecs::Earth, (x_ * scale) * roomScale,
+                    ((y_ * scale - sdlutils().images().at("earth_spike").height()) + h_ * scale) * roomScale,
+                    w_ * scale * roomScale,
+                    h_ * scale * roomScale,
+                    false);
+                interact[std::stoi(ot.getName())].push_back(newSpike);
+                auto v = &interact[std::stoi(ot.getName())];
+                auto it = (--v->end());
                 newSpike->setActive(false);
             }
             else if (ot.getClass() == "Lava") {
@@ -622,11 +634,10 @@ void MapComponent::loadMap(std::string path, int nextPos) {
                     ((y_ * scale - sdlutils().images().at("grass").height()) + h_ * scale) * roomScale,
                     w_ * scale * roomScale,
                     h_ * scale * roomScale,
-                    false);
+                    true);
                 interact[std::stoi(ot.getName())].push_back(newSpike);
                 auto v = &interact[std::stoi(ot.getName())];
                 auto it = (--v->end());
-                //newSpike->getComponent<InteractionComponent>()->setIt(it, v);
                 newSpike->setActive(false);
             }
             else if (classSplit[0] == "Element") {
