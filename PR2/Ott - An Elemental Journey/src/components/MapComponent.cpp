d48433d511c72980b2960e3fd256a4372470d338
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2023-05-05 11:41:44 +0200

    sala de lore 4, pero hace explotar el juego
    me pidio clau que lo subiera, si quereis que no explote borrar el objeto Loretext4 del mapa de fuego :)

--- PR2/Ott - An Elemental Journey/src/components/MapComponent.cpp
+++ PR2/Ott - An Elemental Journey/src/components/MapComponent.cpp
@@ -30,7 +30,7 @@ MapComponent::MapComponent(Entity* fadeOut, PlayState* game, int currentMap) : f
             mapKeys[i].push_back({});
         }
     }
-    currentMapKey = "earthMap";
+    currentMapKey = "fireMap";
     tilemap = &sdlutils().images().at(sdlutils().levels().at(currentMapKey).tileset);
 }
 
